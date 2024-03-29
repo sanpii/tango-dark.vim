4a6e9a19d6ee1bab1085064f064cae5a97fec30a
Sanpi <sanpi@homecomputing.fr>
2024-03-29 18:14:33 +0100

    Exports colors

--- colors/tango.lua
+++ colors/tango.lua
@@ -1,19 +1,4 @@
-local c = {
-    bg = "#000000",
-    blue = "#3465a4",
-    fg = "#ffffff",
-    gray = "#262626",
-    green = "#4e9a06",
-    light_blue = "#729fcf",
-    light_green = "#8ae234",
-    light_purple = "#ad7fa8",
-    light_red = "#ef2929",
-    light_yellow = "#fce94f",
-    orange = "#d75f00",
-    purple = "#75507b",
-    unset = "#ff00ff",
-    yellow = "#c4a000",
-}
+local c = require 'tango.colors'.setup()
 
 function load_syntax()
     return {
---
+++ lua/tango/colors.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+function M.setup()
+    return {
+        bg = "#000000",
+        blue = "#3465a4",
+        fg = "#ffffff",
+        gray = "#262626",
+        green = "#4e9a06",
+        light_blue = "#729fcf",
+        light_green = "#8ae234",
+        light_purple = "#ad7fa8",
+        light_red = "#ef2929",
+        light_yellow = "#fce94f",
+        orange = "#d75f00",
+        purple = "#75507b",
+        unset = "#ff00ff",
+        yellow = "#c4a000",
+    }
+end
+
+return M
