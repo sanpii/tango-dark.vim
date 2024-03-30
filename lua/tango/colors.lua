ba93cbf124f8bdedafd8f988fe583d173ec85031
Sanpi <sanpi@homecomputing.fr>
2024-03-30 08:26:01 +0100

    Adds missing colors

--- lua/tango/colors.lua
+++ lua/tango/colors.lua
@@ -12,8 +12,10 @@ function M.setup()
         light_purple = "#ad7fa8",
         light_red = "#ef2929",
         light_yellow = "#fce94f",
+        light_gray = "#2E3436",
         orange = "#d75f00",
         purple = "#75507b",
+        red = "#CC0000",
         unset = "#ff00ff",
         yellow = "#c4a000",
     }
